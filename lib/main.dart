c019b53b0e346752850e03374e3e6dc45d256ad3
OmarElgammal1 <omarelgammal2004@gmail.com>
2025-04-15 21:30:22 +0200

    Main screen starts with login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
-import 'main_screen.dart';
+import 'login_page.dart';
+import 'dart:io'; // For Platform
+import 'package:sqflite_common_ffi/sqflite_ffi.dart'; // For FFI support
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize SQLite for desktop platforms
+  if (Platform.isWindows || Platform.isLinux) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
   runApp(MyApp());
 }
 
@@ -9,8 +19,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Fashion Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: LoginPage(),
       debugShowCheckedModeBanner: false,
-      home: MainScreen(),
     );
   }
 }
