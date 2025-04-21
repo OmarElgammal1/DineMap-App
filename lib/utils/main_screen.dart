4e6b40b61f54fd59090e88aa9266b86d0ac06d5f
iSeFz <seifyahia@gmail.com>
2025-04-22 01:53:24 +0200

    Renamed wishlist_screen to favorites_screen

--- lib/utils/main_screen.dart
+++ lib/utils/main_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../home_screen.dart';
-import '../wishlist_screen.dart';
+import '../favorites_screen.dart';
 import 'nav_bar.dart';
 
 class MainScreen extends StatefulWidget {
