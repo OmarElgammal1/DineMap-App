64a3ad6516a8c70675fff18487c5ea22ef3a5e7d
OmarElgammal1 <omarelgammal2004@gmail.com>
2025-05-14 17:43:37 +0300

    Add backend urls

---
+++ DineMap-Pro/lib/config/constants.dart
@@ -0,0 +1,5 @@
+// lib/config/constants.dart
+const String API_BASE_URL_ANDROID_EMULATOR = "http://10.0.2.2:5000";
+const String API_BASE_URL_IOS_SIMULATOR = "http://localhost:5000";
+// Choose one based on your testing environment or create a dynamic way to select it.
+const String API_BASE_URL = API_BASE_URL_ANDROID_EMULATOR; // Default to Android emulator
