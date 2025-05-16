ac879fd9e46e9ce9c044cdc523b010b584cea3c9
iSeFz <seifyahia@gmail.com>
2025-05-16 18:56:05 +0300

    Removed unused dependencies from pubspec.yaml Removed old local database configurations Removed unused navigation cubits Removed unused imports from the main file

--- DineMap-Pro/lib/main.dart
+++ DineMap-Pro/lib/main.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'registration/login_page.dart';
-import 'utils/db.dart';
 import 'cubits/store/store_cubit.dart';
 import 'cubits/user/user_cubit.dart';
-import 'cubits/navigation/navigation_cubit.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await DatabaseHelper.initialize(); // Initialize database helper
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,7 +18,6 @@ class MyApp extends StatelessWidget {
       providers: [
         BlocProvider<StoreCubit>(create: (_) => StoreCubit()),
         BlocProvider<UserCubit>(create: (_) => UserCubit()),
-        BlocProvider<NavigationCubit>(create: (_) => NavigationCubit()),
       ],
       child: MaterialApp(
         title: 'Dine Map',
@@ -30,7 +26,7 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        home: LoginPage(),
+        home: const LoginPage(),
         debugShowCheckedModeBanner: false,
       ),
     );
